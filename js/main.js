ae287320d4c6bb9dac37f95b6040af17f1c4c674
Denys Bilonozhko <deformix@gmail.com>
2020-11-11 19:27:41 +0200

    Divide by zero validation

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const operation = document.form.operation;
 const result = document.form.result;
 
 function appendNumber(number) {
+    if (operation.value.length > 15) {
+        operation.style.fontSize = '15px';
+    }
+
+    if (operation.value.length < 15) {
+        operation.style.fontSize = '25px';
+    }
+
     operation.value += number;
 }
 
@@ -23,4 +31,17 @@ equal.addEventListener('click', () => {
     if (operation.value) {
         result.value = parseFloat(eval(operation.value).toPrecision(12));
     }
+
+    if (result.value == Infinity || result.value == -Infinity) {
+        result.value = 'Divide by zero';
+        operation.value = '';
+    }
+
+    if (result.value.length > 8) {
+        result.style.fontSize = '30px';
+    } else {
+        result.style.fontSize = '50px';
+    }
+
+    // result.value.length < 8 ? result.style.fontSize = '50px' :  result.style.fontSize = '30px';
 });
